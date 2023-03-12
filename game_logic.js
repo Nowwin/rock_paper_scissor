ddddb1ed9b5384482fd767b9663fcdf65c192bfa
Abhijeet Chowdhury <abhijeetchowdhury1993@gmail.com>
2023-03-11 20:31:32 -0500

    Implements the final game logic

--- game_logic.js
+++ game_logic.js
@@ -39,3 +39,26 @@ function playRound(playerSelection, computerSelection)  {
         return "You lose! " + computerSelection + " beats " + playerSelection;
     }
 }
+
+function takeUserChoice() {
+    return prompt("Enter your choice! Choices are Rock, Paper and Scissors");
+}
+
+function game() {
+
+    let result = 0;
+
+    for (let index = 0; index < 5; index++) {
+        let userChoice = takeUserChoice();
+        let computerChoice = getComputerChoice();
+        console.log(playRound(userChoice, computerChoice));
+
+        result = result + comparePlayerChoice(userChoice, computerChoice);
+    }
+
+    if (result == 0) {
+        console.log("It's a tie");
+    } else if (result > 0) {
+        console.log("Player won!");
+    } else console.log("Player lost!");
+}
