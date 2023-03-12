c1b816d844b2f530bb9a80fc77433492509cf981
Abhijeet Chowdhury <abhijeetchowdhury1993@gmail.com>
2023-03-11 20:14:05 -0500

    Adds playRound to logic

--- game_logic.js
+++ game_logic.js
@@ -3,3 +3,39 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
   }
+
+function comparePlayerChoice(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return 0;
+    }
+
+    if (playerSelection.toLowerCase() == "rock") {
+        if (computerSelection.toLowerCase() == "paper") {
+            return -1;
+        } else return 1;
+    }
+
+    if (playerSelection.toLowerCase() == "paper") {
+        if (computerSelection.toLowerCase() == "scissors") {
+            return -1;
+        } else return 1;
+    }
+
+    if (playerSelection.toLowerCase() == "scissors") {
+        if (computerSelection.toLowerCase() == "rock") {
+            return -1;
+        } else return 1;
+    }
+}
+
+function playRound(playerSelection, computerSelection)  {
+    result = comparePlayerChoice(playerSelection, computerSelection);
+
+    if (result == 0) {
+        return "It's a tie!"
+    } else if (result == 1) {
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+}
